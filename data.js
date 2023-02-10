3856cc688fe23d44125ba471adeb722b221be008
Nefi Avila <avila.nefi1280@gmail.com>
2023-02-10 02:58:44 -0500

    Proyecto al 45%

---
+++ data.js
@@ -0,0 +1,20 @@
+let allmusic= [
+    {
+        name: 'Believer',
+        artist: 'Imagine Dragons',
+        img: 'image1',
+        src: 'music1'
+    },
+    {
+        name: 'Blinding Lights',
+        artist: 'The Weeknd',
+        img: 'image1',
+        src: 'music1'
+    },
+    {
+        name: 'The Box',
+        artist: 'Roddy Ricch',
+        img: 'image3',
+        src: 'music3'
+    },
+]
